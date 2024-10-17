88da38e6079ee39feadd767013d744ebfbca1a19
lolfh1230 <151025090+lolfh1230@users.noreply.github.com>
2024-10-17 13:15:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+(function($){
+    $(function(){
+        $(".jq--scroll-o-mne").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--o-mne").offset().top}, 800 );
+        });
+    });
+})(jQuery); 
+
+(function($){
+    $(function(){
+        $(".jq--scroll-portfolio").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--portfolio").offset().top}, 800 );
+        });
+
+    });
+})(jQuery); 
+
+
+(function($){
+    $(function(){
+        $(".jq--scroll-vice").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--portfolio").offset().top}, 800 );
+        });
+
+    });
+})(jQuery); 
